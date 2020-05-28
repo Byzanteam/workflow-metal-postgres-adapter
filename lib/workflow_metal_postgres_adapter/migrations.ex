dc243652498bea4cdeabb698e72ae86f1f1933bc
Zheng Jie <zhengj89@icloud.com>
2020-05-28 16:03:58 +0800

    Add migration schema

---
+++ lib/workflow_metal_postgres_adapter/migrations.ex
@@ -0,0 +1,56 @@
+defmodule WorkflowMetalPostgresAdapter.Migrations do
+  @moduledoc false
+
+  use Ecto.Migration
+
+  @initial_version 1
+  @current_version 1
+  @schema Application.get_env(:workflow_metal_postgres_adapter, WorkflowMetalPostgresAdapter)[:schema] || "public"
+  @prefix Application.get_env(:workflow_metal_postgres_adapter, WorkflowMetalPostgresAdapter)[:prefix] || "workflow"
+
+  def up(opts \\ []) when is_list(opts) do
+    schema = @schema
+    prefix = @prefix
+    version = Keyword.get(opts, :version, @current_version)
+    initial = min(migrated_version(repo(), schema, prefix) + 1, @current_version)
+
+    if initial <= version, do: change(schema, prefix, initial..version, :up)
+  end
+
+  def down(opts \\ []) when is_list(opts) do
+    schema = @schema
+    prefix = @prefix
+    version = Keyword.get(opts, :version, @initial_version)
+    initial = max(migrated_version(repo(), schema, prefix), @initial_version)
+
+    if initial >= version, do: change(schema, prefix, initial..version, :down)
+  end
+
+  def initial_version, do: @initial_version
+
+  def current_version, do: @current_version
+
+  def migrated_version(repo, schema, prefix) do
+    query = """
+    SELECT description
+    FROM pg_class
+    LEFT JOIN pg_description ON pg_description.objoid = pg_class.oid
+    LEFT JOIN pg_namespace ON pg_namespace.oid = pg_class.relnamespace
+    WHERE pg_class.relname = '#{prefix}_workflows'
+    AND pg_namespace.nspname = '#{schema}'
+    """
+
+    case repo.query(query) do
+      {:ok, %{rows: [[version]]}} when is_binary(version) -> String.to_integer(version)
+      _ -> 0
+    end
+  end
+
+  defp change(schema, prefix, range, direction) do
+    for index <- range do
+      [__MODULE__, "V#{index}"]
+      |> Module.concat()
+      |> apply(direction, [schema, prefix])
+    end
+  end
+end
---
+++ lib/workflow_metal_postgres_adapter/migrations/v1.ex
@@ -0,0 +1,136 @@
+defmodule WorkflowMetalPostgresAdapter.Migrations.V1 do
+  use Ecto.Migration
+
+  alias WorkflowMetalPostgresAdapter.Schema.{
+    Workflow,
+    Place,
+    Transition,
+    Workitem,
+    Token,
+    Case,
+    Task,
+    Token,
+    Arc
+  }
+
+  alias WorkflowMetalPostgresAdapter.Migrations.Helper
+
+  def up(schema, prefix) do
+    if schema != "public", do: execute("CREATE SCHEMA IF NOT EXISTS #{schema}")
+
+    Workflow.StateEnum.create_type()
+    Place.TypeEnum.create_type()
+    Transition.JoinType.create_type()
+    Transition.SplitType.create_type()
+    Workitem.StateType.create_type()
+    Case.StateType.create_type()
+    Task.StateType.create_type()
+    Token.StateType.create_type()
+    Arc.DirectionType.create_type()
+
+
+    create_if_not_exists table("#{prefix}_workflows", primary_key: false) do
+      add :id, :uuid, primary_key: true
+      add :state, Workflow.StateEnum.type()
+
+      timestamps(updated_at: false)
+    end
+
+    create_if_not_exists table("#{prefix}_places", primary_key: false) do
+      add :id, :uuid, primary_key: true
+      add :workflow_id, :uuid
+      add :type, Place.TypeEnum.type()
+
+      timestamps(updated_at: false)
+    end
+
+    create_if_not_exists table("#{prefix}_transition", primary_key: false) do
+      add :id, :uuid, primary_key: true
+      add :workflow_id, :uuid
+      add :join_type, Transition.JoinType.type()
+      add :split_type, Transition.SplitType.type()
+      add :executor, :string
+      add :executor_params, :map
+
+      timestamps(updated_at: false)
+    end
+
+    create_if_not_exists table("#{prefix}_arcs", primary_key: false) do
+      add :id, :uuid, primary_key: true
+      add :workflow_id, :uuid
+      add :place_id, :uuid
+      add :transition_id, :uuid
+      add :direction, Arc.DirectionType.type()
+
+      timestamps(updated_at: false)
+    end
+
+    create_if_not_exists table("#{prefix}_cases", primary_key: false) do
+      add :id, :uuid, primary_key: true
+      add :workflow_id, :uuid
+      add :state, Case.StateType.type()
+
+      timestamps()
+    end
+
+    create_if_not_exists table("#{prefix}_tasks", primary_key: false) do
+      add :id, :uuid, primary_key: true
+      add :workflow_id, :uuid
+      add :transition_id, :uuid
+      add :case_id, :uuid
+      add :state, Task.StateType.type()
+
+      timestamps()
+    end
+
+    create_if_not_exists table("#{prefix}_tokens", primary_key: false) do
+      add :id, :uuid, primary_key: true
+      add :workflow_id, :uuid
+      add :case_id, :uuid
+      add :place_id, :uuid
+      add :produced_by_task_id, :uuid
+      add :locked_by_task_id, :uuid
+      add :consumed_by_task_id, :uuid
+      add :state, Token.StateType.type()
+      add :payload, :map
+
+      timestamps()
+    end
+
+    create_if_not_exists table("#{prefix}_workitems", primary_key: false) do
+      add :id, :uuid, primary_key: true
+      add :workflow_id, :uuid
+      add :transition_id, :uuid
+      add :case_id, :uuid
+      add :task_id, :uuid
+      add :state, Workitem.StateType.type()
+      add :output, :map
+
+      timestamps()
+    end
+
+    Helper.record_version(schema, prefix, 1)
+  end
+
+  def down(schema, prefix) do
+    drop_if_exists table("#{prefix}_workflows", schema: schema)
+    drop_if_exists table("#{prefix}_places", schema: schema)
+    drop_if_exists table("#{prefix}_transition", schema: schema)
+    drop_if_exists table("#{prefix}_arcs", schema: schema)
+
+    drop_if_exists table("#{prefix}_cases", schema: schema)
+    drop_if_exists table("#{prefix}_tasks", schema: schema)
+    drop_if_exists table("#{prefix}_tokens", schema: schema)
+    drop_if_exists table("#{prefix}_workitems", schema: schema)
+
+    Workflow.StateEnum.drop_type()
+    Place.TypeEnum.drop_type()
+    Transition.JoinType.drop_type()
+    Transition.SplitType.drop_type()
+    Workitem.StateType.drop_type()
+    Token.StateType.drop_type()
+    Case.StateType.drop_type()
+    Task.StateType.drop_type()
+    Arc.DirectionType.drop_type()
+  end
+end
