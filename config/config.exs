d1a62da26b57b80ee0f0d38802598fd8647056df
Zheng Jie <zhengj89@icloud.com>
2020-05-30 22:56:44 +0800

    Add github action db services

--- config/config.exs
+++ config/config.exs
@@ -7,4 +7,13 @@ config :workflow_metal_postgres_adapter, WorkflowMetalPostgresAdapter,
   schema: "public",
   prefix: "workflow_metal"
 
-import_config "#{Mix.env()}.exs"
+config :workflow_metal_postgres_adapter, WorkflowMetalPostgresAdapter.Repo,
+  database: "workflow_metal_postgres_adapter_repo",
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost",
+  pool: Ecto.Adapters.SQL.Sandbox
+
+config :logger,
+  backends: [:console],
+  level: :info
