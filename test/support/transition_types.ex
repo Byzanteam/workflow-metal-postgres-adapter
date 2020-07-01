a486efc32d1a84cf0facfe8354a34960b461e100
CptBreeza <cbasicok@gmail.com>
2020-07-01 14:38:11 +0800

    Add moduledocs

--- test/support/transition_types.ex
+++ test/support/transition_types.ex
@@ -1,11 +1,17 @@
 defmodule WorkflowMetalPostgresAdapter.Support.TransitionTypes do
+  @moduledoc false
+
   defmodule SplitTypeEnum do
+    @moduledoc false
+
     use EctoEnum,
       none: 0,
       and: 1
   end
 
   defmodule JoinTypeEnum do
+    @moduledoc false
+
     use EctoEnum,
       none: 0,
       and: 1
