b9a8855896bb3f64a3b6dbca5e492c3bdf860baf
Phil Chen <06fahchen@gmail.com>
2020-09-28 18:31:22 +0800

    refactor: remove legacy codes

--- lib/workflow_metal_postgres_adapter/repo.ex
+++
@@ -1,8 +0,0 @@
-defmodule WorkflowMetalPostgresAdapter.Repo do
-  @moduledoc """
-  WorkflowMetalPostgresAdapter repo for test.
-  """
-  use Ecto.Repo,
-    otp_app: :workflow_metal_postgres_adapter,
-    adapter: Ecto.Adapters.Postgres
-end
