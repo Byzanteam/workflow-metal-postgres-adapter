367be9260390972dd97cc965ec49072b829c92f7
Phil Chen <06fahchen@gmail.com>
2023-06-16 22:29:45 +0800

    chore: upgrade elixir to 1.14

--- mix.exs
+++ mix.exs
@@ -4,16 +4,14 @@ defmodule WorkflowMetalPostgresAdapter.MixProject do
   def project do
     [
       app: :workflow_metal_postgres_adapter,
-      version: "0.2.0",
-      elixir: "~> 1.10",
+      version: "0.3.0",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       build_per_environment: is_nil(System.get_env("GITHUB_ACTIONS")),
       elixirc_paths: elixirc_paths(Mix.env()),
       aliases: aliases(),
-      dialyzer: [
-        plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
-      ]
+      dialyzer: [plt_file: {:no_warn, "priv/plts/dialyzer.plt"}]
     ]
   end
 
@@ -22,31 +20,35 @@ defmodule WorkflowMetalPostgresAdapter.MixProject do
     [extra_applications: application(Mix.env())]
   end
 
-  defp application(:test), do: [:postgrex, :ecto, :logger]
-  defp application(_), do: [:logger]
+  defp application(:test) do
+    [:postgrex, :ecto, :logger]
+  end
+
+  defp application(_) do
+    [:logger]
+  end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 1.2", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.0.0-rc.7", only: [:dev], runtime: false},
       {:doctor, "~> 0.12.0", only: [:dev]},
-      {:ecto, "~> 3.5-pre"},
-      {:ecto_sql, "~> 3.5-pre"},
+      {:ecto_sql, "~> 3.9"},
       {:jason, "~> 1.1"},
-      {:postgrex, "~> 0.14"},
-      {:workflow_metal, github: "Byzanteam/workflow_metal", branch: "master"}
+      {:postgrex, "~> 0.16"},
+      {:workflow_metal, [github: "Byzanteam/workflow_metal", branch: "main"]},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:jet_credo, [github: "Byzanteam/jet_credo", only: [:dev, :test], runtime: false]},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
     ]
   end
 
-  defp elixirc_paths(:test),
-    do: [
-      "lib",
-      "test/support",
-      "test/helpers"
-    ]
+  defp elixirc_paths(:test) do
+    ["lib", "test/support", "test/helpers"]
+  end
 
-  defp elixirc_paths(_), do: ["lib"]
+  defp elixirc_paths(_) do
+    ["lib"]
+  end
 
   defp aliases do
     [
